112bb9c05562bcf5b1bda9cdbf5ec8f17e44d46a
Matt Gebert <mattheuu.gebert@gmail.com>
2026-03-24 12:49:42 +1100

    refactor(utils.py): add methods for energy wavelenth conversion
    makes code cleaer instead of assuming correct use of math

--- XEFI/utils.py
+++ XEFI/utils.py
@@ -6,6 +6,8 @@ Includes external dependencies and helper functions for the XEFI package.
 
 # External dependencies
 import scipy.constants as sc
+import numpy.typing as npt
+import typing
 
 # Check for optional dependencies
 HAS_KKCALC: bool
@@ -19,7 +21,7 @@ except ImportError:
     HAS_KKCALC = False
 
 
-en2wav: float = sc.h * sc.c / sc.e * 1e10
+_en2wav_factor: float = sc.h * sc.c / sc.e * 1e10
 r"""
 Conversion factor from energy in eV to wavelength in angstroms.
 
@@ -28,7 +30,8 @@ Conversion factor from energy in eV to wavelength in angstroms.
     wav = en2wav / E
 
 """
-en2wvec: float = 2 * sc.pi / en2wav
+
+_en2wvec_factor: float = 2 * sc.pi / _en2wav_factor
 r"""
 Conversion factor from energy in eV to wavevector in inverse angstroms.
 
@@ -38,3 +41,73 @@ Conversion factor from energy in eV to wavevector in inverse angstroms.
     \k = 2 \pi / (\lambda)
     \k = en2wvec * E
 """
+
+T = typing.TypeVar("T", bound=npt.ArrayLike)
+
+
+def en2wav(energy: T) -> T:
+    r"""
+    Convert energy in eV to wavelength in angstroms.
+
+    Parameters
+    ----------
+    energy : npt.ArrayLike
+        Energy in eV to be converted to wavelength in angstroms.
+
+    Returns
+    -------
+    npt.ArrayLike
+        Wavelength in angstroms corresponding to the input energy in eV.
+    """
+    return _en2wav_factor / energy
+
+
+def en2wvec(energy: T) -> T:
+    r"""
+    Convert energy in eV to wavevector in inverse angstroms.
+
+    Parameters
+    ----------
+    energy : npt.ArrayLike
+        Energy in eV to be converted to wavevector in inverse angstroms.
+
+    Returns
+    -------
+    npt.ArrayLike
+        Wavevector in inverse angstroms corresponding to the input energy in eV.
+    """
+    return _en2wvec_factor * energy
+
+
+def wav2en(wavelength: T) -> T:
+    r"""
+    Convert wavelength in angstroms to energy in eV.
+
+    Parameters
+    ----------
+    wavelength : npt.ArrayLike
+        Wavelength in angstroms to be converted to energy in eV.
+
+    Returns
+    -------
+    npt.ArrayLike
+        Energy in eV corresponding to the input wavelength in angstroms.
+    """
+    return _en2wav_factor / wavelength
+
+
+def wvec2en(wvec: T) -> T:
+    r"""
+    Convert wavevector in inverse angstroms to energy in eV.
+
+    Parameters
+    ----------
+    wvec : npt.ArrayLike
+        Wavevector in inverse angstroms to be converted to energy in eV.
+
+    Returns
+    -------
+    npt.ArrayLike
+        Energy in eV corresponding to the input wavevector in inverse angstroms.
+    """
+    return wvec / _en2wvec_factor
